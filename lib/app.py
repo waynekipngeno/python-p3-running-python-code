4e6df335e4310f9b8e205efb2cf677121155f83d
Wayne Korir <kipngenowayne@gmail.com>
2023-05-15 20:16:14 +0300

    Creating a Python Application Assignment

---
+++ lib/app.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+# testing ending types of the print function
+# the print operations below include a newline character /n by default
+print("Hello world!") 
+print("Hello sun!")
+print("Hello sky!")
+
+# Choosing your own print() Ending
+
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello sun!", end="**")
+print("Hello sky!", end="!!!\n")
